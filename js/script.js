b16c62850ac0d9ef83a914c2b2b92f90787b260a
ASM-Monirul-Islam <monirul.01401411046@gmail.com>
2026-02-16 20:12:35 +0600

    added responsiveness to the hero section by monirul

--- js/script.js
+++ js/script.js
@@ -26,3 +26,20 @@ document.addEventListener("click", (e)=>{
 		navbar.classList.remove("active");
 	}
 });
+
+
+// hero image
+
+// const images = [
+//     "../assets/hero1.png",
+//     "../assets/hero2.png"
+// ];
+
+// const hero = document.getElementById("hero_img");
+
+// let index = 0;
+
+// setInterval(() => {
+//     index = (index + 1) % images.length;
+//     hero.style.backgroundImage = `url(${images[index]})`;
+// }, 2000);
